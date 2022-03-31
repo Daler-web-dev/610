067b955eb7dc9a9969934fb44a82cbc628839aac
Daler <dsarifkulo021@gmail.com>
2022-03-31 11:42:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// методы работ с типом данных намбер
+// github
+
+
+let a = 7
+let b = 2.25329984239423
+
+console.log(
+    // Math.floor(i) // to down
+    // Math.ceil(i) // to up 
+    // Math.round(i) // logical
+
+    // parseInt(i)
+    // parseFloat(i)
+    // +i
+    // Number(i)
+
+    // Math.max(a, b, 122, 09,2212)
+    // Math.min(a, b, 122, 09,2212)
+    // Math.pow(a, b) // **
+
+    // b.toFixed(4)
+);
